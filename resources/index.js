0c2d479c46603fb14336cabbf751a2d83858fb03
sabellnikov <sabellnikov@gmail.com>
2025-07-15 15:23:23 +0300

    fix

--- resources/index.js
+++
@@ -1,7 +0,0 @@
-<script>
-  // Check if URL ends with ".html"
-  if (window.location.pathname.endsWith('.html')) {
-    // Redirect to the same page without ".html"
-    window.location.href = window.location.pathname.replace('.html', '');
-  }
-</script>
