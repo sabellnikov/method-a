728d5c32f820e5073c3848fd272fa84076da4692
sabellnikov <sabellnikov@gmail.com>
2025-09-17 11:47:26 +0300

    .

--- resources/js/read/nav-back.js
+++
@@ -1,7 +0,0 @@
-function goToPreviousPage() {
-  if (document.referrer && new URL(document.referrer).pathname !== location.pathname) {
-    location.href = document.referrer;
-  } else {
-    location.href = '/';
-  }
-}
